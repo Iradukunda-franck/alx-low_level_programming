7022fa9dc4544847b22b3ed492ccddf8524354d8
Yola-p <irafranckchinois@gmail.com>
2023-02-16 13:43:23 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
 # include <stdio.h>
 /**
- *  main - A program that prints a line with puts function
- *  return: 0 (successs)
+ *  * main - Entry point of the program
+ *  Return: 0 if successful, non-zero value if there was an error
  */
 int main(void)
 {
