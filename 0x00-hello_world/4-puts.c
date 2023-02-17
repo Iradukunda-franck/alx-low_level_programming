b1d0fc9f2b29199be052a4f2369c40ea4c2d3e48
Yola-p <irafranckchinois@gmail.com>
2023-02-16 16:32:12 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,10 +2,10 @@
 /**
  * main - prints ""Programming is like building a multilingual puzzle,
  * followed by a new line."
- * Return: 0.
+ * Return: Always 0.
  */
 int main(void)
 {
-puts("\Programming is like building a multilingual puzzle");
+puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
