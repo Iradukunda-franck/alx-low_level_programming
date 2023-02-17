8ccd930ff400f5425e6054f58cda47a4713feabb
Yola-p <irafranckchinois@gmail.com>
2023-02-16 16:06:40 -0800

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - prints "and that piece of art is useful" - Dora Korpar, 2015-10-19",
+ *  followed by a new line, to the standard error.
+ * Return: always.
+ */
+int main(void)
+{
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	return (1);
+}
