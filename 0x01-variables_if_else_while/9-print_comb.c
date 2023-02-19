7075b365f575ea7d224dda46e5ac4ef99771c806
Yola-p <irafranckchinois@gmail.com>
2023-02-19 15:42:06 -0800

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -10,18 +10,18 @@ int main(void)
 {
 int i = 0;
 
-while (i++ < 89)
+while (i < 10)
 {
-putchar((i / 10) + '0');
-putchar((i % 10) + '0');
-if (i != 89)
+putchar(i + '0');
+if (i != 9)
 {
 putchar(',');
 putchar(' ');
 }
+i++;
 }
-
 putchar('\n');
+
 
 return (0);
 }
