f70fb9ce8966cc48a8ee845c1f00e23082fcc8ce
Yola-p <irafranckchinois@gmail.com>
2023-02-21 13:53:18 -0800

    -alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * print_alphabet - print all alphabet in lowercase
+ */
+int main(void)
+
+
+void print_alphabet(void)
+{
+char letter = 'a';
+while (letter <= 'z')
+{
+putchar(letter);
+letter++;
+}
+putchar('\n');
+}
