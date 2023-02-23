fd1ca27c207b5a23fd10a00f8ffb88db37b436a9
Yola-p <irafranckchinois@gmail.com>
2023-02-23 11:08:10 -0800

    task3

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -1,3 +1,6 @@
+#include <stdio.h>
+#include "main.h"
+
 /**
 * print_remaining_days - takes a date and prints how many days are
 * left in the year, taking leap years into account
@@ -9,32 +12,27 @@
 
 void print_remaining_days(int month, int day, int year)
 {
-int days_in_month[] = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-int day_count = 0;
-
-if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0)
+if ((year % 4 == 0 || year % 400 == 0) && !(year % 100 == 0))
 {
-days_in_month[2] = 29;
+if (month >= 2 && day >= 60)
+{
+day++;
 }
 
-for (int i = 1; i < month; i++)
-{
-day_count += days_in_month[i];
-}
-day_count += day;
-
-if (month > 12 || day > days_in_month[month])
-{
-printf("Invalid date: %02d/%02d/%04d\n", month, day, year);
-}
-else if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0)
-{
-printf("Day of the year: %d\n", day_count);
-printf("Remaining days: %d\n", 366 - day_count);
+printf("Day of the year: %d\n", day);
+printf("Remaining days: %d\n", 366 - day);
 }
 else
 {
-printf("Day of the year: %d\n", day_count);
-printf("Remaining days: %d\n", 365 - day_count);
+if (month == 2 && day == 60)
+{
+printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
+}
+else
+{
+printf("Day of the year: %d\n", day);
+printf("Remaining days: %d\n", 365 - day);
+        }
 }
 }
+
