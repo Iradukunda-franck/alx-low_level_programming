74696e52bd470f723617ae29dcbc96948d4724d3
Yola-p <irafranckchinois@gmail.com>
2023-04-05 09:07:39 -0700

    task 9

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -4,32 +4,34 @@
 #include "lists.h"
 
 /**
- * main - entry point for the program
+ * main - check the code
  *
- * Return: Always 0
+ * Return: Always 0.
  */
 int main(void)
 {
-    listint_t *head;
+	listint_t *head;
 
-    head = NULL;
-    add_nodeint_end(&head, 0);
-    add_nodeint_end(&head, 1);
-    add_nodeint_end(&head, 2);
-    add_nodeint_end(&head, 3);
-    add_nodeint_end(&head, 4);
-    add_nodeint_end(&head, 98);
-    add_nodeint_end(&head, 402);
-    add_nodeint_end(&head, 1024);
+	head = NULL;
+	add_nodeint_end(&head, 0);
+	add_nodeint_end(&head, 1);
+	add_nodeint_end(&head, 2);
+	add_nodeint_end(&head, 3);
+	add_nodeint_end(&head, 4);
+	add_nodeint_end(&head, 98);
+	add_nodeint_end(&head, 402);
+	add_nodeint_end(&head, 1024);
 
-    print_listint(head);
-    printf("-----------------\n");
+	print_listint(head);
 
-    insert_nodeint_at_index(&head, 5, 4096);
+	printf("-----------------\n");
 
-    print_listint(head);
+	insert_nodeint_at_index(&head, 5, 4096);
 
-    free_listint2(&head);
+	print_listint(head);
 
-    return (0);
+	free_listint2(&head);
+
+	return (0);
 }
+
